1102a478fe9a114bdf1cd1a7661cf6b5df088fcc
MajorHodl <115755916+MajorHodl@users.noreply.github.com>
2024-03-01 19:11:07 -0500

    Update preloader.js src

--- preloader.js
+++ preloader.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://github.com/MajorHodl/bhopesalonloadingscript/blob/main/preloadBlack.svg';
+svgImage.src = 'https://raw.githubusercontent.com/MajorHodl/bhopesalonloadingscript/main/preloadBlack.svg';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
